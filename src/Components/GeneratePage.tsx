1c073277db80e172db31aef91286a1f12bdf126f
Andrew Kim <andrewkim09@hotmail.com>
2025-04-14 18:57:53 -0400

    changed fetch for ngrok backend hosting

--- src/Components/GeneratePage.tsx
+++ src/Components/GeneratePage.tsx
@@ -160,7 +160,7 @@ export const GeneratePage: React.FC<GeneratePageProps> = ({db, storage}) => {
     }
 
     try{
-    const response = await fetch("http://localhost:5000/separate", {
+    const response = await fetch("https://7732-2607-fea8-54e4-ae00-a52a-ed61-1a1a-fcd2.ngrok-free", {
       method: "POST",
       body: formData,
     }).finally(() => setProcessing(false))
